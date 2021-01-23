1f6767f12af9fa9ae8e4db0779b74067a19a8631
Dinh Anh <dinhanhx@gmail.com>
2021-01-23 10:27:13 +0700

    Remove unused import

--- app/src/main/java/vn/edu/usth/flickr/LoginActivity.java
+++ app/src/main/java/vn/edu/usth/flickr/LoginActivity.java
@@ -4,13 +4,8 @@ package vn.edu.usth.flickr;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-
-import com.codepath.oauth.OAuthLoginActivity;
-
-import vn.edu.usth.flickr.MainActivity;
-import vn.edu.usth.flickr.R;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
