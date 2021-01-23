c3c1a5f36b206f974deb4ddd8e17171f6c451f34
Dinh Anh <dinhanhx@gmail.com>
2021-01-23 09:29:57 +0700

    OAuth now works I haven't passed Auth user data to other fragments

--- app/src/main/java/vn/edu/usth/flickr/LoginActivity.java
+++ app/src/main/java/vn/edu/usth/flickr/LoginActivity.java
@@ -15,6 +15,7 @@ import com.flickr4java.flickr.REST;
 import com.flickr4java.flickr.auth.Auth;
 import com.flickr4java.flickr.auth.AuthInterface;
 import com.flickr4java.flickr.auth.Permission;
+import com.github.scribejava.core.model.OAuth1AccessToken;
 import com.github.scribejava.core.model.OAuth1RequestToken;
 import com.github.scribejava.core.model.OAuth1Token;
 
@@ -25,17 +26,19 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.ExecutionException;
+
 public class LoginActivity extends AppCompatActivity {
     SharedPreferences sp;
 
     private final String apiKey = "b73450b170505cfc1020ef349a0a8cb4";
     private final String sharedSecret = "679e34e410b67118";
     private Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
+    AuthInterface authInterface = flickr.getAuthInterface();
+    OAuth1RequestToken requestToken;
+    Auth auth;
 
     String tokenKey;
-    OAuth1RequestToken requestToken;
-    AuthInterface authInterface;
-    Auth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,21 +51,40 @@ public class LoginActivity extends AppCompatActivity {
         if(sp.getBoolean("logged",false)){
             goToMainActivity();
         } else {
-            getTokenTask ft = new getTokenTask();
-            ft.execute();
+            getTokenTask gtt = new getTokenTask();
+            gtt.execute();
 
             final EditText tokenEditText = (EditText) findViewById(R.id.token);
-
             final Button loginButton = (Button) findViewById(R.id.sign_in);
 
             loginButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     tokenKey = tokenEditText.getText().toString();
-                    authTask at = new authTask();
-                    at.execute();
 
-                    updateUiWithUser(tokenEditText.getText().toString());
+                    // do the magic here
+                    AsyncTask<Void, Void, Void> authTask = new AsyncTask<Void, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Void... voids) {
+                            OAuth1AccessToken accessToken = (OAuth1AccessToken) authInterface.getAccessToken(requestToken, tokenKey);
+                            try {
+                                auth = authInterface.checkToken(accessToken);
+                            } catch (FlickrException e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
+                    };
+
+                    try {
+                        authTask.execute().get();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    updateUiWithUser(auth.getUser().getUsername());
                     sp.edit().putBoolean("logged", true).apply();
                     goToMainActivity();
                     finish();
@@ -79,42 +101,20 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUiWithUser(String name) {
-        String welcome = "Welcome ";
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
     }
 
     private class getTokenTask extends AsyncTask<Void, Void, String> {
 
         @Override
-        protected void onPreExecute(){
-            Toast.makeText(getBaseContext(), "Opening browser. Please wait.", Toast.LENGTH_LONG).show();
-        }
-
-        @Override
         protected String doInBackground(Void... voids) {
-            authInterface = flickr.getAuthInterface();
             requestToken = authInterface.getRequestToken();
             return authInterface.getAuthorizationUrl(requestToken, Permission.DELETE);
         }
 
         @Override
-        protected void onPostExecute(String url) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-        }
-    }
-
-    private class authTask extends AsyncTask<Void, Void, Void>{
-        @Override
-        protected Void doInBackground(Void... voids){
-            Log.i("haha ded",requestToken.toString());
-            try {
-                OAuth1Token accessToken = authInterface.getAccessToken(
-                        requestToken, tokenKey);
-                auth = authInterface.checkToken(accessToken);
-            } catch (FlickrException e) {
-                e.printStackTrace();
-            }
-            return null;
+        protected void onPostExecute(String s) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(s)));
         }
     }
 }
